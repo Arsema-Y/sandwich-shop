4100792196fb1f527e79ce0197e1033481b6ba96
mili Arsema <meleymm88@gmail.com>
2026-05-26 11:20:05 -0500

    adds task list

---
+++ src/main/java/com/bigbacks/TaskList.java
@@ -0,0 +1,50 @@
+package com.bigbacks;
+
+import java.util.List;
+
+public record TaskList() {
+    /*TASK-LIST.;
+    ARCHITECTURE ===============================
+    - packages ☑️
+    - classes ☑️
+    - guide comment
+
+    MODEL ==============================
+    - Interface
+    - Abstract
+    - Heirs
+      - Sandwich ------------------
+        = fields
+        = methods
+      - Sides ------------------
+        = fields
+        = methods
+      - Drinks ------------------
+        = fields
+        = methods
+      - Orders ------------------
+        = fields
+        = methods
+
+    UI =================================
+    - Menu
+        = fields
+        = methods
+          - screen-1 ------------------
+          - screen-2 ------------------
+          - screen-3 ------------------
+          - screen-4 ------------------
+          - screen-5 ------------------
+          - screen-6 ------------------
+        = Edits
+
+    DATA ============================
+    - OrderHistory
+        = method
+        = exception
+
+
+    APP ===========================
+    - main method
+     */
+}
